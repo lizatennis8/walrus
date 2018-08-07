f3f062489e06ac8c0356435cb0ffd220f32ca2e7
Krystal Foster and Liza Tennis <liza@>
2018-08-07 14:34:27 -0700

    add buttons and make them functional with prepend

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").toggle();
-    $("#initially-hidden").toggle();
+    `$("#initially-showing").toggle();
+    $("#initially-hidden").toggle();`
   });
   $("button#green").click(function() {
     $("body").removeClass();
@@ -18,5 +18,17 @@ $(document).ready(function() {
 $("button#nighttime").click(function() {
   $("body").removeClass();
   $("body").addClass("black-background");
+});
+$("button#1").click(function() {
+  $("ul").prepend("<li>This is right?</li>")
+});
+$("button#2").click(function() {
+  $("ul").prepend("<li>FINALLY</li>")
+});
+$("button#3").click(function() {
+  $("ul").prepend("<li>Fine!</li>")
+});
+$("button#4").click(function() {
+  $("ul").prepend("<li>Yes, ma'am :3</li>")
 });
 });
