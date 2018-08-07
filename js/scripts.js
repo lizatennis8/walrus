d650feb1a31f3ab339bd0e858d4cf774cb800ac8
Krystal Foster and Liza Tennis <liza@>
2018-08-07 09:56:56 -0700

    add buttons, background colors and remove class function for toggling

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
     $("#initially-showing").toggle();
     $("#initially-hidden").toggle();
   });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
