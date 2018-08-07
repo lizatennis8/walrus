f8b21da2fe2b415f8b23af47570eef76236ec242
Krystal Foster and Liza Tennis <liza@>
2018-08-07 15:39:21 -0700

    add remove elements to click functions on buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -20,19 +20,47 @@ $("button#nighttime").click(function() {
   $("body").addClass("black-background");
 });
 $("button#1").click(function() {
-  $("ul#user").prepend("<li>This is right?</li>")
-  $("ul#webpage").prepend("<li>Yes?</li>")
+  $("ul#user").prepend("<li>This is right?</li>");
+  $("ul#webpage").prepend("<li>Yes?</li>");
+
+  $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+  });
+  $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+  });
 });
 $("button#2").click(function() {
-  $("ul#user").prepend("<li>Hey, girrrl</li>")
-  $("ul#webpage").prepend("<li>I have no gender. Does not compute.</li>")
+  $("ul#user").prepend("<li>Hey, girrrl</li>");
+  $("ul#webpage").prepend("<li>I have no gender. Does not compute.</li>");
+
+  $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+  });
+  $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+  });
 });
 $("button#3").click(function() {
-  $("ul#user").prepend("<li>Get out!</li>")
-  $("ul#webpage").prepend("<li>Ok.</li>")
+  $("ul#user").prepend("<li>Get out!</li>");
+  $("ul#webpage").prepend("<li>Ok.</li>");
+
+  $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+  });
+  $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+  });
 });
 $("button#4").click(function() {
-  $("ul#user").prepend("<li>No, come back!</li>")
-  $("ul#webpage").prepend("<li>I never really left.</li>")
-});
-});
+  $("ul#user").prepend("<li>No, come back!</li>");
+  $("ul#webpage").prepend("<li>I never really left.</li>");
+
+  $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+  });
+  $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+  });
+    });
+  });
